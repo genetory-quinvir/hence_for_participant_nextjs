81d9a3715cf817af8fe87707b2fec280db80906e
Genetory <genetory@jiyouns-MacBook-Pro.local>
2025-08-08 11:35:57 +0900

    업데이트

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  // 정적 배포를 위한 설정
-  ...(process.env.NODE_ENV === 'production' && {
-    output: 'export',
-  }),
 };
 
 export default nextConfig;
